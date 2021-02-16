cc3b1d784737144251d78de6e2dd981889c0d7bf
Michael Millar <mwmichael+sc@protonmail.ch>
2021-02-16 10:48:17 +0000

    source in sh

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 python -m venv venv
-source venv/bin/activate
+. venv/bin/activate
 pip install --upgrade pip
 pip install requests
+pip install pytest
 pip install pip-tools pip-chill
 pip-compile requirements.in
 pip-sync
